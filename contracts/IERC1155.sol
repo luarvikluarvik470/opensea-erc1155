3ef88122d14bb72372eb0e01d0d68f9cdf69adec
luarvikluarvik470 <57499924+luarvikluarvik470@users.noreply.github.com>
2020-03-15 07:49:29 +0800

    Initial Commit

--- contracts/IERC1155.sol
+++ contracts/IERC1155.sol
@@ -1,90 +1,197 @@
 pragma solidity ^0.5.16;
 
+import "../../interfaces/IERC165.sol";
+import "../../utils/SafeMath.sol";
+import "../../interfaces/IERC1155TokenReceiver.sol";
+import "../../interfaces/IERC1155.sol";
+import "../../utils/Address.sol";
 
-interface IERC1155 {
 
-  /****************************************|
-  |                 Events                 |
-  |_______________________________________*/
+/**
+ * @dev Implementation of Multi-Token Standard contract
+ */
+contract ERC1155 is IERC165, IERC1155 {
+  using SafeMath for uint256;
+  using Address for address;
+
+  /***********************************|
+  |        Variables and Events       |
+  |__________________________________*/
+
+  // onReceive function signatures
+  bytes4 constant internal ERC1155_RECEIVED_VALUE = 0xf23a6e61;
+  bytes4 constant internal ERC1155_BATCH_RECEIVED_VALUE = 0xbc197c81;
+
+  // Objects balances
+  mapping (address => mapping(uint256 => uint256)) internal balances;
+
+  // Operator Functions
+  mapping (address => mapping(address => bool)) internal operators;
+
+
+  /***********************************|
+  |     Public Transfer Functions     |
+  |__________________________________*/
 
   /**
-   * @dev Either TransferSingle or TransferBatch MUST emit when tokens are transferred, including zero amount transfers as well as minting or burning
-   *   Operator MUST be msg.sender
-   *   When minting/creating tokens, the `_from` field MUST be set to `0x0`
-   *   When burning/destroying tokens, the `_to` field MUST be set to `0x0`
-   *   The total amount transferred from address 0x0 minus the total amount transferred to 0x0 may be used by clients and exchanges to be added to the "circulating supply" for a given token ID
-   *   To broadcast the existence of a token ID with no initial balance, the contract SHOULD emit the TransferSingle event from `0x0` to `0x0`, with the token creator as `_operator`, and a `_amount` of 0
+   * @notice Transfers amount amount of an _id from the _from address to the _to address specified
+   * @param _from    Source address
+   * @param _to      Target address
+   * @param _id      ID of the token type
+   * @param _amount  Transfered amount
+   * @param _data    Additional data with no specified format, sent in call to `_to`
    */
-  event TransferSingle(address indexed _operator, address indexed _from, address indexed _to, uint256 _id, uint256 _amount);
+  function safeTransferFrom(address _from, address _to, uint256 _id, uint256 _amount, bytes memory _data)
+    public
+  {
+    require((msg.sender == _from) || isApprovedForAll(_from, msg.sender), "ERC1155#safeTransferFrom: INVALID_OPERATOR");
+    require(_to != address(0),"ERC1155#safeTransferFrom: INVALID_RECIPIENT");
+    // require(_amount <= balances[_from][_id]) is not necessary since checked with safemath operations
+
+    _safeTransferFrom(_from, _to, _id, _amount);
+    _callonERC1155Received(_from, _to, _id, _amount, gasleft(), _data);
+  }
 
   /**
-   * @dev Either TransferSingle or TransferBatch MUST emit when tokens are transferred, including zero amount transfers as well as minting or burning
-   *   Operator MUST be msg.sender
-   *   When minting/creating tokens, the `_from` field MUST be set to `0x0`
-   *   When burning/destroying tokens, the `_to` field MUST be set to `0x0`
-   *   The total amount transferred from address 0x0 minus the total amount transferred to 0x0 may be used by clients and exchanges to be added to the "circulating supply" for a given token ID
-   *   To broadcast the existence of multiple token IDs with no initial balance, this SHOULD emit the TransferBatch event from `0x0` to `0x0`, with the token creator as `_operator`, and a `_amount` of 0
+   * @notice Send multiple types of Tokens from the _from address to the _to address (with safety call)
+   * @param _from     Source addresses
+   * @param _to       Target addresses
+   * @param _ids      IDs of each token type
+   * @param _amounts  Transfer amounts per token type
+   * @param _data     Additional data with no specified format, sent in call to `_to`
    */
-  event TransferBatch(address indexed _operator, address indexed _from, address indexed _to, uint256[] _ids, uint256[] _amounts);
+  function safeBatchTransferFrom(address _from, address _to, uint256[] memory _ids, uint256[] memory _amounts, bytes memory _data)
+    public
+  {
+    // Requirements
+    require((msg.sender == _from) || isApprovedForAll(_from, msg.sender), "ERC1155#safeBatchTransferFrom: INVALID_OPERATOR");
+    require(_to != address(0), "ERC1155#safeBatchTransferFrom: INVALID_RECIPIENT");
+
+    _safeBatchTransferFrom(_from, _to, _ids, _amounts);
+    _callonERC1155BatchReceived(_from, _to, _ids, _amounts, gasleft(), _data);
+  }
+
+
+  /***********************************|
+  |    Internal Transfer Functions    |
+  |__________________________________*/
 
   /**
-   * @dev MUST emit when an approval is updated
+   * @notice Transfers amount amount of an _id from the _from address to the _to address specified
+   * @param _from    Source address
+   * @param _to      Target address
+   * @param _id      ID of the token type
+   * @param _amount  Transfered amount
    */
-  event ApprovalForAll(address indexed _owner, address indexed _operator, bool _approved);
+  function _safeTransferFrom(address _from, address _to, uint256 _id, uint256 _amount)
+    internal
+  {
+    // Update balances
+    balances[_from][_id] = balances[_from][_id].sub(_amount); // Subtract amount
+    balances[_to][_id] = balances[_to][_id].add(_amount);     // Add amount
+
+    // Emit event
+    emit TransferSingle(msg.sender, _from, _to, _id, _amount);
+  }
 
   /**
-   * @dev MUST emit when the URI is updated for a token ID
-   *   URIs are defined in RFC 3986
-   *   The URI MUST point a JSON file that conforms to the "ERC-1155 Metadata JSON Schema"
+   * @notice Verifies if receiver is contract and if so, calls (_to).onERC1155Received(...)
    */
-  event URI(string _amount, uint256 indexed _id);
-
-
-  /****************************************|
-  |                Functions               |
-  |_______________________________________*/
-
-  /**
-    * @notice Transfers amount of an _id from the _from address to the _to address specified
-    * @dev MUST emit TransferSingle event on success
-    * Caller must be approved to manage the _from account's tokens (see isApprovedForAll)
-    * MUST throw if `_to` is the zero address
-    * MUST throw if balance of sender for token `_id` is lower than the `_amount` sent
-    * MUST throw on any other error
-    * When transfer is complete, this function MUST check if `_to` is a smart contract (code size > 0). If so, it MUST call `onERC1155Received` on `_to` and revert if the return amount is not `bytes4(keccak256("onERC1155Received(address,address,uint256,uint256,bytes)"))`
-    * @param _from    Source address
-    * @param _to      Target address
-    * @param _id      ID of the token type
-    * @param _amount  Transfered amount
-    * @param _data    Additional data with no specified format, sent in call to `_to`
-    */
-  function safeTransferFrom(address _from, address _to, uint256 _id, uint256 _amount, bytes calldata _data) external;
+  function _callonERC1155Received(address _from, address _to, uint256 _id, uint256 _amount, uint256 _gasLimit, bytes memory _data)
+    internal
+  {
+    // Check if recipient is contract
+    if (_to.isContract()) {
+      bytes4 retval = IERC1155TokenReceiver(_to).onERC1155Received.gas(_gasLimit)(msg.sender, _from, _id, _amount, _data);
+      require(retval == ERC1155_RECEIVED_VALUE, "ERC1155#_callonERC1155Received: INVALID_ON_RECEIVE_MESSAGE");
+    }
+  }
 
   /**
-    * @notice Send multiple types of Tokens from the _from address to the _to address (with safety call)
-    * @dev MUST emit TransferBatch event on success
-    * Caller must be approved to manage the _from account's tokens (see isApprovedForAll)
-    * MUST throw if `_to` is the zero address
-    * MUST throw if length of `_ids` is not the same as length of `_amounts`
-    * MUST throw if any of the balance of sender for token `_ids` is lower than the respective `_amounts` sent
-    * MUST throw on any other error
-    * When transfer is complete, this function MUST check if `_to` is a smart contract (code size > 0). If so, it MUST call `onERC1155BatchReceived` on `_to` and revert if the return amount is not `bytes4(keccak256("onERC1155BatchReceived(address,address,uint256[],uint256[],bytes)"))`
-    * Transfers and events MUST occur in the array order they were submitted (_ids[0] before _ids[1], etc)
-    * @param _from     Source addresses
-    * @param _to       Target addresses
-    * @param _ids      IDs of each token type
-    * @param _amounts  Transfer amounts per token type
-    * @param _data     Additional data with no specified format, sent in call to `_to`
-  */
-  function safeBatchTransferFrom(address _from, address _to, uint256[] calldata _ids, uint256[] calldata _amounts, bytes calldata _data) external;
+   * @notice Send multiple types of Tokens from the _from address to the _to address (with safety call)
+   * @param _from     Source addresses
+   * @param _to       Target addresses
+   * @param _ids      IDs of each token type
+   * @param _amounts  Transfer amounts per token type
+   */
+  function _safeBatchTransferFrom(address _from, address _to, uint256[] memory _ids, uint256[] memory _amounts)
+    internal
+  {
+    require(_ids.length == _amounts.length, "ERC1155#_safeBatchTransferFrom: INVALID_ARRAYS_LENGTH");
+
+    // Number of transfer to execute
+    uint256 nTransfer = _ids.length;
+
+    // Executing all transfers
+    for (uint256 i = 0; i < nTransfer; i++) {
+      // Update storage balance of previous bin
+      balances[_from][_ids[i]] = balances[_from][_ids[i]].sub(_amounts[i]);
+      balances[_to][_ids[i]] = balances[_to][_ids[i]].add(_amounts[i]);
+    }
+
+    // Emit event
+    emit TransferBatch(msg.sender, _from, _to, _ids, _amounts);
+  }
+
+  /**
+   * @notice Verifies if receiver is contract and if so, calls (_to).onERC1155BatchReceived(...)
+   */
+  function _callonERC1155BatchReceived(address _from, address _to, uint256[] memory _ids, uint256[] memory _amounts, uint256 _gasLimit, bytes memory _data)
+    internal
+  {
+    // Pass data if recipient is contract
+    if (_to.isContract()) {
+      bytes4 retval = IERC1155TokenReceiver(_to).onERC1155BatchReceived.gas(_gasLimit)(msg.sender, _from, _ids, _amounts, _data);
+      require(retval == ERC1155_BATCH_RECEIVED_VALUE, "ERC1155#_callonERC1155BatchReceived: INVALID_ON_RECEIVE_MESSAGE");
+    }
+  }
+
+
+  /***********************************|
+  |         Operator Functions        |
+  |__________________________________*/
+
+  /**
+   * @notice Enable or disable approval for a third party ("operator") to manage all of caller's tokens
+   * @param _operator  Address to add to the set of authorized operators
+   * @param _approved  True if the operator is approved, false to revoke approval
+   */
+  function setApprovalForAll(address _operator, bool _approved)
+    external
+  {
+    // Update operator status
+    operators[msg.sender][_operator] = _approved;
+    emit ApprovalForAll(msg.sender, _operator, _approved);
+  }
+
+  /**
+   * @notice Queries the approval status of an operator for a given owner
+   * @param _owner     The owner of the Tokens
+   * @param _operator  Address of authorized operator
+   * @return True if the operator is approved, false if not
+   */
+  function isApprovedForAll(address _owner, address _operator)
+    public view returns (bool isOperator)
+  {
+    return operators[_owner][_operator];
+  }
+
+
+  /***********************************|
+  |         Balance Functions         |
+  |__________________________________*/
 
   /**
    * @notice Get the balance of an account's Tokens
    * @param _owner  The address of the token holder
    * @param _id     ID of the Token
-   * @return        The _owner's balance of the Token type requested
+   * @return The _owner's balance of the Token type requested
    */
-  function balanceOf(address _owner, uint256 _id) external view returns (uint256);
+  function balanceOf(address _owner, uint256 _id)
+    public view returns (uint256)
+  {
+    return balances[_owner][_id];
+  }
 
   /**
    * @notice Get the balance of multiple account/token pairs
@@ -92,21 +199,53 @@ interface IERC1155 {
    * @param _ids    ID of the Tokens
    * @return        The _owner's balance of the Token types requested (i.e. balance for each (owner, id) pair)
    */
-  function balanceOfBatch(address[] calldata _owners, uint256[] calldata _ids) external view returns (uint256[] memory);
+  function balanceOfBatch(address[] memory _owners, uint256[] memory _ids)
+    public view returns (uint256[] memory)
+  {
+    require(_owners.length == _ids.length, "ERC1155#balanceOfBatch: INVALID_ARRAY_LENGTH");
+
+    // Variables
+    uint256[] memory batchBalances = new uint256[](_owners.length);
+
+    // Iterate over each owner and token ID
+    for (uint256 i = 0; i < _owners.length; i++) {
+      batchBalances[i] = balances[_owners[i]][_ids[i]];
+    }
+
+    return batchBalances;
+  }
+
+
+  /***********************************|
+  |          ERC165 Functions         |
+  |__________________________________*/
 
   /**
-   * @notice Enable or disable approval for a third party ("operator") to manage all of caller's tokens
-   * @dev MUST emit the ApprovalForAll event on success
-   * @param _operator  Address to add to the set of authorized operators
-   * @param _approved  True if the operator is approved, false to revoke approval
+   * INTERFACE_SIGNATURE_ERC165 = bytes4(keccak256("supportsInterface(bytes4)"));
    */
-  function setApprovalForAll(address _operator, bool _approved) external;
+  bytes4 constant private INTERFACE_SIGNATURE_ERC165 = 0x01ffc9a7;
 
   /**
-   * @notice Queries the approval status of an operator for a given owner
-   * @param _owner     The owner of the Tokens
-   * @param _operator  Address of authorized operator
-   * @return           True if the operator is approved, false if not
+   * INTERFACE_SIGNATURE_ERC1155 =
+   * bytes4(keccak256("safeTransferFrom(address,address,uint256,uint256,bytes)")) ^
+   * bytes4(keccak256("safeBatchTransferFrom(address,address,uint256[],uint256[],bytes)")) ^
+   * bytes4(keccak256("balanceOf(address,uint256)")) ^
+   * bytes4(keccak256("balanceOfBatch(address[],uint256[])")) ^
+   * bytes4(keccak256("setApprovalForAll(address,bool)")) ^
+   * bytes4(keccak256("isApprovedForAll(address,address)"));
    */
-  function isApprovedForAll(address _owner, address _operator) external view returns (bool isOperator);
+  bytes4 constant private INTERFACE_SIGNATURE_ERC1155 = 0xd9b67a26;
+
+  /**
+   * @notice Query if a contract implements an interface
+   * @param _interfaceID  The interface identifier, as specified in ERC-165
+   * @return `true` if the contract implements `_interfaceID` and
+   */
+  function supportsInterface(bytes4 _interfaceID) external view returns (bool) {
+    if (_interfaceID == INTERFACE_SIGNATURE_ERC165 ||
+        _interfaceID == INTERFACE_SIGNATURE_ERC1155) {
+      return true;
+    }
+    return false;
+  }
 }
